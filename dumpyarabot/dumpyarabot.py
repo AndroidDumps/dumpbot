06c4ed30b0f84ef317f14815bec0443c5a751200
yshalsager <ysh-alsager@hotmail.com>
2020-05-05 16:27:00 +0530

    dumpyarabot: add support for mega urls
    They generally are of the format `mega.nz/#!<id>`
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- dumpyarabot/dumpyarabot.py
+++ dumpyarabot/dumpyarabot.py
@@ -16,7 +16,7 @@ DP = Dispatcher(BOT)
 @DP.message_handler(commands=['dump'], commands_prefix='/', user_id=ALLOWED_USERS, chat_id=ALLOWED_CHATS)
 async def jenkins_bridge(message: types.Message):
     pattern = re.compile(
-        r"(https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))")
+        r"(https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*))")
     url = pattern.search(message.text)
     if not url:
         await message.answer("Not a valid URL!")
