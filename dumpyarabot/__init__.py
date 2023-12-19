4c2f964abbd2f8dc81da16d98b5d65a5cdb74e84
Akhil Narang <me@akhilnarang.dev>
2023-12-19 15:31:10 +0530

    chore: only log >=warning for httpx
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- dumpyarabot/__init__.py
+++ dumpyarabot/__init__.py
@@ -4,5 +4,6 @@ from rich.logging import RichHandler
 
 FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=FORMAT, handlers=[RichHandler()])
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger("rich")
