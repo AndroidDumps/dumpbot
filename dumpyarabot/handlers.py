efe77045b1cec50d175a33e8e7e1ef329d625460
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-09-24 00:55:16 +0530

    improve code style
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- dumpyarabot/handlers.py
+++ dumpyarabot/handlers.py
@@ -1,6 +1,8 @@
+from typing import Optional
+
 from pydantic import ValidationError
 from rich.console import Console
-from telegram import Update
+from telegram import Chat, Message, Update
 from telegram.ext import ContextTypes
 
 from dumpyarabot import schemas, utils
@@ -12,19 +14,18 @@ console = Console()
 async def dump_main(
     update: Update, context: ContextTypes.DEFAULT_TYPE, use_alt_dumper: bool = False
 ) -> None:
-    # Just here to keep mypy happy
-    if (
-        update.effective_chat is None
-        or update.effective_message is None
-        or update.effective_user is None
-    ):
-        raise Exception("What happened here?")
+    """Main handler for the /dump and /dump_alt commands."""
+    chat: Optional[Chat] = update.effective_chat
+    message: Optional[Message] = update.effective_message
+
+    if not chat or not message:
+        return
 
     # Ensure it can only be used in the correct group
-    if update.effective_chat.id not in settings.ALLOWED_CHATS:
+    if chat.id not in settings.ALLOWED_CHATS:
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            reply_to_message_id=update.effective_message.id,
+            chat_id=chat.id,
+            reply_to_message_id=message.message_id,
             text="You can't use this here",
         )
         return
@@ -32,66 +33,76 @@ async def dump_main(
     # Ensure that we had some arguments passed
     if not context.args:
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            reply_to_message_id=update.effective_message.id,
+            chat_id=chat.id,
+            reply_to_message_id=message.message_id,
             text="Please pass in a URL",
         )
         return
 
     # Try to call jenkins
     try:
-        response_text = await utils.call_jenkins(
-            schemas.DumpArguments(url=context.args[0], use_alt_dumper=use_alt_dumper)
+        dump_args = schemas.DumpArguments(
+            url=context.args[0], use_alt_dumper=use_alt_dumper
         )
+
+        response_text = await utils.call_jenkins(dump_args)
     except ValidationError:
         response_text = "Invalid URL"
     except Exception:
-        response_text = "Exception occurred"
+        response_text = "An error occurred"
         console.print_exception(show_locals=True)
 
     # Reply to the user with whatever the status is
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        reply_to_message_id=update.effective_message.id,
+        chat_id=chat.id,
+        reply_to_message_id=message.message_id,
         text=response_text,
     )
 
 
 async def dump(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handler for the /dump command."""
     await dump_main(update, context, use_alt_dumper=True)
 
 
 async def dump_alt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Handler for the /dump_alt command."""
     await dump_main(update, context, use_alt_dumper=False)
 
 
 async def cancel_dump(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Just here to keep mypy happy
-    if (
-        update.effective_chat is None
-        or update.effective_message is None
-        or update.effective_user is None
-    ):
-        raise Exception("What happened here?")
+    """Handler for the /cancel_dump command."""
+    chat: Optional[Chat] = update.effective_chat
+    message: Optional[Message] = update.effective_message
+    user = update.effective_user
 
-    if (
-        update.effective_chat.id not in settings.ALLOWED_CHATS
-        or update.effective_user
-        not in (
-            admin.user for admin in await update.effective_chat.get_administrators()
-        )
-    ):
+    if not chat or not message or not user:
+        return
+
+    # Ensure it can only be used in the correct group
+    if chat.id not in settings.ALLOWED_CHATS:
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            reply_to_message_id=update.effective_message.id,
+            chat_id=chat.id,
+            reply_to_message_id=message.message_id,
             text="You can't use this here",
         )
         return
 
+    # Check if the user is an admin
+    admins = await chat.get_administrators()
+    if user not in [admin.user for admin in admins]:
+        await context.bot.send_message(
+            chat_id=chat.id,
+            reply_to_message_id=message.message_id,
+            text="You don't have permission to use this command",
+        )
+        return
+
+    # Ensure that we had some arguments passed
     if not context.args:
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            reply_to_message_id=update.effective_message.id,
+            chat_id=chat.id,
+            reply_to_message_id=message.message_id,
             text="Please provide a job ID.",
         )
         return
@@ -99,7 +110,7 @@ async def cancel_dump(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     job_id = context.args[0]
     response_message = await utils.cancel_jenkins_job(job_id)
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        reply_to_message_id=update.effective_message.id,
+        chat_id=chat.id,
+        reply_to_message_id=message.message_id,
         text=response_message,
     )
--- dumpyarabot/utils.py
+++ dumpyarabot/utils.py
@@ -5,35 +5,22 @@ from dumpyarabot.config import settings
 
 
 async def call_jenkins(args: schemas.DumpArguments) -> str:
-    """
-    Function to call jenkins
-
-    :param args: The schema for the jenkins call
-    :return: A reply for the user
-    """
+    """Call Jenkins to start a new build."""
     async with httpx.AsyncClient() as client:
         response = await client.post(
             f"{settings.JENKINS_URL}/job/dumpyara/buildWithParameters",
-            params=httpx.QueryParams(
-                {
-                    "URL": args.url.unicode_string(),
-                    "USE_ALT_DUMPER": args.use_alt_dumper,
-                }
-            ),
+            params={
+                "URL": args.url.unicode_string(),
+                "USE_ALT_DUMPER": args.use_alt_dumper,
+            },
             auth=(settings.JENKINS_USER_NAME, settings.JENKINS_USER_TOKEN),
         )
-        if response.status_code in (200, 201):
-            return "Job started"
-        return response.text
+        response.raise_for_status()
+        return "Job started"
 
 
 async def cancel_jenkins_job(job_id: str) -> str:
-    """
-    Function to cancel a Jenkins job.
-
-    :param job_id: The ID of the Jenkins job to cancel
-    :return: A reply for the user
-    """
+    """Cancel a Jenkins job."""
     async with httpx.AsyncClient() as client:
         response = await client.post(
             f"{settings.JENKINS_URL}/job/dumpyara/{job_id}/stop",
@@ -45,11 +32,10 @@ async def cancel_jenkins_job(job_id: str) -> str:
         elif response.status_code == 404:
             response = await client.post(
                 f"{settings.JENKINS_URL}/queue/cancelItem",
-                params=httpx.QueryParams({"id": job_id}),
+                params={"id": job_id},
                 auth=(settings.JENKINS_USER_NAME, settings.JENKINS_USER_TOKEN),
                 follow_redirects=True,
             )
             if response.status_code == 204:
                 return f"Job with ID {job_id} has been removed from the queue."
-        print(response.text)
-        return f"Failed to cancel job with ID {job_id}. Status code: {response.status_code}. Check stdout for more details."
+        return f"Failed to cancel job with ID {job_id}. Status code: {response.status_code}."
